36aa3a43c02060d9e202cd792c3cc5abdcd7af93
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-06 12:36:01 -0300

    feat; adding user repository

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Repositories.User;
+
+namespace MyRecipeBook.Infrastructure.DataAccess.Repositories
+{
+    public class UserRepository : IUserWriteOnlyRepository, IUserReadOnlyRepository
+    {
+        private readonly MyRecipeBookDbContext _dbContext;
+
+        public UserRepository(MyRecipeBookDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task Add(User user)
+        {
+            await _dbContext.Users.AddAsync(user);
+        }
+
+        public async Task<bool> ExistActiveUserWithEmail(string email) => await _dbContext.Users.AnyAsync(u => u.Email.Equals(email) && u.Active);
+
+        public async Task<User?> GetByEmailAndPassword(string email, string password)
+        {
+            return await _dbContext
+                .Users
+                .AsNoTracking() // cannot be edited
+                .FirstOrDefaultAsync(u => u.Active && u.Email.Equals(email) && u.Password.Equals(password));
+        }
+    }
+}
