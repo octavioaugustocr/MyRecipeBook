f3be29fadd327793a67c882d8a791b62986aab2d
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-07 16:41:33 -0300

    feat: adding password validator

---
+++ src/Backend/MyRecipeBook.Application/SharedValidators/ValidatorsExtensions.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation;
+
+namespace MyRecipeBook.Application.SharedValidators
+{
+    public static class ValidatorsExtensions
+    {
+        public static IRuleBuilderOptions<T, string> PasswordIsValid<T>(this IRuleBuilder<T, string> ruleBuilder) => ruleBuilder.SetValidator(new PasswordValidator<T>());
+    }
+}
