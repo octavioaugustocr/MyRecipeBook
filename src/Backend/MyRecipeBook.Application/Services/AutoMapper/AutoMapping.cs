027ec78f3bf20aee62557d936c2effc8d457f1a4
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-08 13:34:12 -0300

    feat: adding recipes mapper

--- src/Backend/MyRecipeBook.Application/Services/AutoMapper/AutoMapping.cs
+++ src/Backend/MyRecipeBook.Application/Services/AutoMapper/AutoMapping.cs
@@ -1,13 +1,19 @@
 ﻿using AutoMapper;
+using MyRecipeBook.Communication.Enums;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
+using Sqids;
 
 namespace MyRecipeBook.Application.Services.AutoMapper
 {
     public class AutoMapping : Profile
     {
-        public AutoMapping()
+        private readonly SqidsEncoder<long> _idEncoder;
+
+        public AutoMapping(SqidsEncoder<long> idEncoder)
         {
+            _idEncoder = idEncoder;
+
             RequestToDomain();
             DomainToResponse();
         }
@@ -16,11 +22,27 @@ namespace MyRecipeBook.Application.Services.AutoMapper
         {
             CreateMap<RequestRegisterUserJson, Domain.Entities.User>()
                 .ForMember(dest => dest.Password, opt => opt.Ignore());
+
+            CreateMap<RequestRecipeJson, Domain.Entities.Recipe>()
+                .ForMember(dest => dest.Instructions, opt => opt.Ignore())
+                .ForMember(dest => dest.Ingredients, opt => opt.MapFrom(source => source.Ingredients.Distinct()))
+                .ForMember(dest => dest.DishTypes, opt => opt.MapFrom(source => source.DishTypes.Distinct()));
+
+            CreateMap<string, Domain.Entities.Ingredient>()
+                .ForMember(dest => dest.Item, opt => opt.MapFrom(source => source));
+
+            CreateMap<DishType, Domain.Entities.DishType>()
+                .ForMember(dest => dest.Type, opt => opt.MapFrom(source => source));
+
+            CreateMap<RequestInstructionJson, Domain.Entities.Instruction>();
         }
 
         private void DomainToResponse()
         {
             CreateMap<Domain.Entities.User, ResponseUserProfileJson>();
+
+            CreateMap<Domain.Entities.Recipe, ResponseRegisteredRecipeJson>()
+                .ForMember(dest => dest.Id, config => config.MapFrom(recipe => _idEncoder.Encode(recipe.Id)));
         }
     }
 }
