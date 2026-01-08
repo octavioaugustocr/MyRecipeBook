7ad458a47f690fba806d49fe9ff41fe117ee59f2
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-08 13:38:54 -0300

    feat: adding recipe controller

---
+++ src/Backend/MyRecipeBook.API/Controllers/RecipeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.API.Atributes;
+using MyRecipeBook.Application.UseCases.Recipe;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.API.Controllers
+{
+    [AuthenticatedUser]
+    public class RecipeController : MyRecipeBookBaseController
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredRecipeJson), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Register([FromServices] IRegisterRecipeUseCase useCase, [FromBody] RequestRecipeJson request)
+        {
+            var response = await useCase.Execute(request);
+            
+            return Created(string.Empty, response);
+        }
+    }
+}
