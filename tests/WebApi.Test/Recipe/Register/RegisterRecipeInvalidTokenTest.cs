c29d30b556a82e0b1a18ef229c33c867654d9df8
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-09 16:52:18 -0300

    test: adding register recipe invalid token test

---
+++ tests/WebApi.Test/Recipe/Register/RegisterRecipeInvalidTokenTest.cs
@@ -0,0 +1,46 @@
+﻿using CommonTestUtilities.Tokens;
+using FluentAssertions;
+using MyRecipeBook.Communication.Requests;
+using System.Net;
+
+namespace WebApi.Test.Recipe.Register
+{
+    public class RegisterRecipeInvalidTokenTest : MyRecipeBookClassFixture
+    {
+        private const string METHOD = "recipe";
+
+        public RegisterRecipeInvalidTokenTest(CustomWebApplicationFactory factory) : base(factory) { }
+
+        [Fact]
+        public async Task Error_Token_Invalid()
+        {
+            var request = new RequestRecipeJson();
+
+            var response = await DoPost(method: METHOD, request: request, token: "invalidToken");
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Error_Without_Token()
+        {
+            var request = new RequestRecipeJson();
+
+            var response = await DoPost(method: METHOD, request: request, token: string.Empty);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Error_Token_With_User_NotFound()
+        {
+            var token = JwtTokenGeneratorBuilder.Build().Generate(Guid.NewGuid());
+
+            var request = new RequestRecipeJson();
+
+            var response = await DoPost(method: METHOD, request: request, token: token);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+    }
+}
