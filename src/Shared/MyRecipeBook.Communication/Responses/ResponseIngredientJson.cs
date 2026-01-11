b220ec8e68fa134be1b40bbcbc3d5e60760deebb
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-11 14:18:46 -0300

    feat: adding response ingredient

---
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseIngredientJson.cs
@@ -0,0 +1,8 @@
+﻿namespace MyRecipeBook.Communication.Responses
+{
+    public class ResponseIngredientJson
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Item { get; set; } = string.Empty;
+    }
+}
