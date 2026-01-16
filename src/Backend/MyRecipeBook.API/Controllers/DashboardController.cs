fdf72df4398c8c8022a6023cac7d6b9425948206
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-16 10:19:50 -0300

    feat: adding endpoint to dashboard

---
+++ src/Backend/MyRecipeBook.API/Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.API.Atributes;
+using MyRecipeBook.Application.UseCases.Dashboard;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.API.Controllers
+{
+    [AuthenticatedUser]
+    public class DashboardController : MyRecipeBookBaseController
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(ResponseRecipesJson), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> Get([FromServices] IGetDashboardUseCase useCase)
+        {
+            var response = await useCase.Execute();
+
+            if (response.Recipes.Any())
+                return Ok(response.Recipes);
+
+            return NoContent();
+        }
+    }
+}
