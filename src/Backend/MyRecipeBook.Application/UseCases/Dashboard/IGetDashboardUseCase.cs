b727f6fe78ae587d3f29d3fe848529a9a45383f6
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-16 10:16:47 -0300

    feat: adding get dashboard use case

---
+++ src/Backend/MyRecipeBook.Application/UseCases/Dashboard/IGetDashboardUseCase.cs
@@ -0,0 +1,9 @@
+﻿using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.Application.UseCases.Dashboard
+{
+    public interface IGetDashboardUseCase
+    {
+        public Task<ResponseRecipesJson> Execute();
+    }
+}
