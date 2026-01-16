f3b3a2259a6d276a03e0f0840c55edebd2f4bf35
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-16 10:22:41 -0300

    test: adding get dashboard invalid token test

---
+++ tests/WebApi.Test/Dashboard/GetDashboardInvalidTokenTest.cs
@@ -0,0 +1,41 @@
+﻿using CommonTestUtilities.Tokens;
+using FluentAssertions;
+using System.Net;
+
+namespace WebApi.Test.Dashboard
+{
+    public class GetDashboardInvalidTokenTest : MyRecipeBookClassFixture
+    {
+        private const string METHOD = "dashboard";
+
+        public GetDashboardInvalidTokenTest(CustomWebApplicationFactory factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Error_Token_Invalid()
+        {
+            var response = await DoGet(METHOD, token: "tokenInvalid");
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Error_Without_Token()
+        {
+            var response = await DoGet(METHOD, string.Empty);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Error_Token_With_User_NotFound()
+        {
+            var token = JwtTokenGeneratorBuilder.Build().Generate(Guid.NewGuid());
+
+            var response = await DoGet(METHOD, token);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+    }
+}
