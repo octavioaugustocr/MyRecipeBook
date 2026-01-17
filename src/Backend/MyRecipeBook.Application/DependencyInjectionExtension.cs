752ab8e84c052bfd4d5fe4f8bdcec21663d0177b
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-17 11:47:56 -0300

    feat: adding update image cover use case

--- src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
+++ src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -6,6 +6,7 @@ using MyRecipeBook.Application.UseCases.Recipe.Delete;
 using MyRecipeBook.Application.UseCases.Recipe.Filter;
 using MyRecipeBook.Application.UseCases.Recipe.Generate;
 using MyRecipeBook.Application.UseCases.Recipe.GetById;
+using MyRecipeBook.Application.UseCases.Recipe.Image;
 using MyRecipeBook.Application.UseCases.Recipe.Register;
 using MyRecipeBook.Application.UseCases.Recipe.Update;
 using MyRecipeBook.Application.UseCases.User.ChangePassword;
@@ -61,6 +62,7 @@ namespace MyRecipeBook.Application
             services.AddScoped<IUpdateRecipeUseCase, UpdateRecipeUseCase>();
             services.AddScoped<IGetDashboardUseCase, GetDashboardUseCase>();
             services.AddScoped<IGenerateRecipeUseCase, GenerateRecipeUseCase>();
+            services.AddScoped<IAddUpdateImageCoverUseCase, AddUpdateImageCoverUseCase>();
         }
     }
 }
---
+++ src/Backend/MyRecipeBook.Application/UseCases/Recipe/Image/AddUpdateImageCoverUseCase.cs
@@ -0,0 +1,59 @@
+﻿using FileTypeChecker.Extensions;
+using FileTypeChecker.Types;
+using Microsoft.AspNetCore.Http;
+using MyRecipeBook.Domain.Extensions;
+using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.Recipe;
+using MyRecipeBook.Domain.Services.LoggedUser;
+using MyRecipeBook.Domain.Services.Storage;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+
+namespace MyRecipeBook.Application.UseCases.Recipe.Image
+{
+    public class AddUpdateImageCoverUseCase : IAddUpdateImageCoverUseCase
+    {
+        private readonly ILoggedUser _loggedUser;
+        private readonly IRecipeUpdateOnlyRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IBlobStorageService _blobStorageService;
+
+        public AddUpdateImageCoverUseCase(ILoggedUser loggedUser, IRecipeUpdateOnlyRepository repository, IUnitOfWork unitOfWork, IBlobStorageService blobStorageService)
+        {
+            _loggedUser = loggedUser;
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+            _blobStorageService = blobStorageService;
+        }
+
+        public async Task Execute(long recipeId, IFormFile file)
+        {
+            var loggedUser = await _loggedUser.User();
+
+            var recipe = await _repository.GetById(loggedUser, recipeId);
+
+            if (recipe is null)
+                throw new NotFoundException(ResourceMessagesException.RECIPE_NOT_FOUND);
+
+            var fileStream = file.OpenReadStream();
+
+            if (fileStream.Is<PortableNetworkGraphic>().IsFalse() && fileStream.Is<JointPhotographicExpertsGroup>().IsFalse())
+            {
+                throw new ErrorOnValidationException([ResourceMessagesException.ONLY_IMAGES_ACCEPTED]);
+            }
+
+            if (string.IsNullOrEmpty(recipe.ImageIdentifier))
+            {
+                recipe.ImageIdentifier = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+
+                _repository.Update(recipe);
+
+                await _unitOfWork.Commit();
+            }
+
+            fileStream.Position = 0;
+
+            await _blobStorageService.Upload(loggedUser, fileStream, recipe.ImageIdentifier);
+        }
+    }
+}
