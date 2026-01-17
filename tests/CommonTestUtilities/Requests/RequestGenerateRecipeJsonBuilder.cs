db2a9a5bb933988561be21322d1ecf13bbc8d399
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-17 09:33:36 -0300

    test: adding common test utilities to generate recipe

---
+++ tests/CommonTestUtilities/Requests/RequestGenerateRecipeJsonBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using MyRecipeBook.Communication.Requests;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestGenerateRecipeJsonBuilder
+    {
+        public static RequestGenerateRecipeJson Build(int count = 5)
+        {
+            return new Faker<RequestGenerateRecipeJson>()
+                .RuleFor(recipe => recipe.Ingredients, faker => faker.Make(count, () => faker.Commerce.ProductName()));
+        }
+    }
+}
