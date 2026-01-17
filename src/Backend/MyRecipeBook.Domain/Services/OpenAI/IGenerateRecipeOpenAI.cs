f8a77d83f3daf9f14bd86997b36773164e60d524
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-17 09:30:40 -0300

    feat: adding openAI

---
+++ src/Backend/MyRecipeBook.Domain/Services/OpenAI/IGenerateRecipeOpenAI.cs
@@ -0,0 +1,9 @@
+﻿using MyRecipeBook.Domain.Dtos;
+
+namespace MyRecipeBook.Domain.Services.OpenAI
+{
+    public interface IGenerateRecipeOpenAI
+    {
+        Task<GeneratedRecipeDto> Generate(IList<string> ingredients);
+    }
+}
