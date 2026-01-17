6eb5af71f835c9144d59d15822a14c2ba993d882
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-16 21:33:38 -0300

    feat: adding responses to recipe generated

---
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseGeneratedInstructionJson.cs
@@ -0,0 +1,8 @@
+﻿namespace MyRecipeBook.Communication.Responses
+{
+    public class ResponseGeneratedInstructionJson
+    {
+        public int Step { get; set; }
+        public string Text { get; set; } = string.Empty;
+    }
+}
