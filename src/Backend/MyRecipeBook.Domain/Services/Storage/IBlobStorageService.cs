aadeae06689735603560e964c50c4ea6f23bec48
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-17 11:54:36 -0300

    feat: adding interface blob storage service

---
+++ src/Backend/MyRecipeBook.Domain/Services/Storage/IBlobStorageService.cs
@@ -0,0 +1,9 @@
+﻿using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Domain.Services.Storage
+{
+    public interface IBlobStorageService
+    {
+        Task Upload(User user, Stream file, string fileName);
+    }
+}
