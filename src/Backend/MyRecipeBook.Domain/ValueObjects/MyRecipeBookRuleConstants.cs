7723d821dfd26010ed1bad8ec0e7fd899d438e8f
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-22 18:15:29 -0300

    feat: adding refresh token generator

--- src/Backend/MyRecipeBook.Domain/ValueObjects/MyRecipeBookRuleConstants.cs
+++ src/Backend/MyRecipeBook.Domain/ValueObjects/MyRecipeBookRuleConstants.cs
@@ -4,6 +4,7 @@
     {
         public const int MAXIMUM_INGREDIENTS_GENERATE_RECIPE = 5;
         public const int MAXIMUM_IMAGE_URL_LIFETIME_IN_MINUTES = 10;
+        public const int REFRESH_TOKEN_EXPIRATION_DAYS = 7;
         public const string CHAT_MODEL = "gpt-4o";
     }
 }
