c2b4b538a4431f81e430d58f750035fe7ea44946
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-22 18:12:50 -0300

    feat: adding use refresh token use case

---
+++ src/Backend/MyRecipeBook.Application/UseCases/Token/IUseRefreshTokenUseCase.cs
@@ -0,0 +1,10 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.Application.UseCases.Token
+{
+    public interface IUseRefreshTokenUseCase
+    {
+        Task<ResponseTokensJson> Execute(RequestNewTokenJson request);
+    }
+}
